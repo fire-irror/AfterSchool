1ac5d4d12a888c4cec86a2e228e783db07edfcb6
parkhwakyoung <firenoah.m@gmail.com>
2022-12-26 11:45:53 +0900

    [문법]

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <SFML/GpuPreference.hpp>
 
-int main() {
-	printf("Hello world");
+using namespace sf;
+
+int main(void) {
+	//윈도우 창 생성
+	RenderWindow window(VideoMode(640, 480), "AfterSchool");
+
+	//윈도우가 열려있을 때 까지 반복
+	while (window.isOpen()) {
+		
+		Event event;
+		while (window.pollEvent(event)) {
+			//종료(x)버튼을 누르면  
+			if (event.type == Event::Closed)
+				window.close(); //윈도우 창 닫기
+		}
+	}
+	return 0;
 }
