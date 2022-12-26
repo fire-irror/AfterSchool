f92510d5b552d8c5333ef026b1a40d8719a77f1b
parkhwakyoung <firenoah.m@gmail.com>
2022-12-26 13:53:49 +0900

    사각형 그리기

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -7,15 +7,25 @@ int main(void) {
 	//윈도우 창 생성
 	RenderWindow window(VideoMode(640, 480), "AfterSchool");
 
+	RectangleShape player;
+	player.setSize(Vector2f(40, 40));
+	player.setPosition(100, 100);
+	window.draw(player);
+
 	//윈도우가 열려있을 때 까지 반복
 	while (window.isOpen()) {
 		
 		Event event;
 		while (window.pollEvent(event)) {
+			
 			//종료(x)버튼을 누르면  
-			if (event.type == Event::Closed)
-				window.close(); //윈도우 창 닫기
+			switch (event.type) {
+			case Event::Closed:
+			window. close();
+			}
 		}
+		window.draw(player);
+		window.display();
 	}
 	return 0;
 }
