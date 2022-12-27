14914d2f1a43b950961a4ec4e62a306ff4e80b66
parkhwakyoung <firenoah.m@gmail.com>
2022-12-27 11:16:10 +0900

    [문법] 문자열 덧붙이기4

--- Grammar/문자열 덧붙이기.c
+++ Grammar/문자열 덧붙이기.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h> //itoa사용
 
 int main() {
 
 	char str[30] = "SCORE : ";
-	char str2[30] = "300";
+	int score = 300;
+	
+	char str2[30];
+	//정수를 문자열로 (변환할 정수, 적용대상, 진수)
+	itoa(score, str2, 10);
+
 	strcat(str, str2);
 	printf("%s", str);
 
-	int score = 300;
+
 	return 0;
 	
 
