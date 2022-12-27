80179aeb464c206c9ca049712186166188c8b2ca
parkhwakyoung <firenoah.m@gmail.com>
2022-12-27 11:40:43 +0900

    [문법] 문자열 붙이기 최종

--- Grammar/문자열 덧붙이기.c
+++ Grammar/문자열 덧붙이기.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h> //itoa사용
+#include <string.h>//문자열 라이브러리
+//#include <stdlib.h>// itoa사용
 
 int main() {
 
-	char str[30];
-	int score = 300;
 
-	//문자열에 출력
-	sprint(str, "score : %d", score);
-	
-	printf("%s", str);
+    char str[30];
+    int score = 300;
 
+    sprintf(str, "score : %d", score);
+    printf("%s", str);
 
-	return 0;
-	
+    return 0;
 
 }
