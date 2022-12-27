6445d1bc8716cd870f8d47d2300de07365144ece
parkhwakyoung <firenoah.m@gmail.com>
2022-12-27 10:44:43 +0900

    [문법] 문자열 덧붙이기2

--- Grammar/문자열 덧붙이기.c
+++ Grammar/문자열 덧붙이기.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 	char str[30] = "score : ";
-	int str_len = 0;
-	while (str[str_len] != '\0')
-		str_len++;
+	int str_len = strlen(str);
 
 	char str2[30] = "300";
-	int str2_len = 0;
-	while (str[str2_len] != '\0')
-		str2_len++;
-
+	int str2_len = strlen(str2);
+	
 	for (int i = 0; i < str2_len; i++)
 		str[i + str_len] = str2[i];
 	str[str_len + str2_len] = '\0';
